95593144c4de890725a4d80418199fb834b30fd1
valery <valery.fremaux@gmail.com>
2016-09-27 17:14:14 +0200

    automated sync from SVN

--- lang/en/tool_cronmonitor.php
+++ lang/en/tool_cronmonitor.php
@@ -27,7 +27,5 @@ $string['pluginname'] = 'Cron monitor';
 $string['configpositivemail'] = 'Send positive mail';
 $string['configpositivemail_desc'] = 'If enabled, sends a mail even on check success';
 $string['configuserstosendto'] = 'Usernames to send to';
-$string['configuserstosendto_desc'] = '
-Give a coma separated list of usernames, or full qualified username@mnethostid identities
-to send mails to. when empty, will notify all site admins.
-';
+$string['configuserstosendto_desc'] = 'Give a coma separated list of usernames, or full qualified username@mnethostid identities
+to send mails to. when empty, will notify all site admins.';
--- settings.php
+++ settings.php
@@ -15,10 +15,10 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package     tool_cronmonitor
- * @category    tool
- * @copyright   2016 Valery Fremaux <valery@edunao.com>
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @package    tool_cronmonitor
+ * @category   tool
+ * @copyright  2016 Valery Fremaux <valery@edunao.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
