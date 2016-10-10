8ac866c4ff7c5783d6013b11ab87660150277866
valery <valery.fremaux@gmail.com>
2016-10-10 20:49:31 +0200

    Automated sync from SVN

--- lang/fr/tool_cronmonitor.php
+++ lang/fr/tool_cronmonitor.php
@@ -25,7 +25,7 @@
 
 $string['pluginname'] = 'Moniteur de cron';
 $string['configpositivemail'] = 'Envoyer les notifications positives';
-$string['configpositivemail_desc'] = 'Si activé, le moniteur émet un coaurriel même en cas de succès de la vérification';
+$string['configpositivemail_desc'] = 'Si activé, le moniteur émet un courriel même en cas de succès de la vérification';
 $string['configuserstosendto'] = 'Utilisateurs à notifier';
 $string['configuserstosendto_desc'] = '
 Donnez une liste séparée par des virgules d\'identifants utilisateur, ou une entrée complètement qualifiée <username>@<mnethostid>.
