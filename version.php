0fcaec0bdaf42f4fb793e55a52d09185382bce70
valery <valery.fremaux@gmail.com>
2018-02-28 10:56:12 +0100

    add cron tail abstract on general error detection

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018022800; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016112900; // Requires this Moodle version.
+$plugin->requires  = 2017050500; // Requires this Moodle version.
 $plugin->component = 'tool_cronmonitor'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.2.0 (Build 2018022800)';
+$plugin->release = '3.3.0 (Build 2018022800)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0001';
+$plugin->codeincrement = '3.3.0001';
