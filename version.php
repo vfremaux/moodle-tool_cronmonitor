34f894ebfeda8569eb0556526d8cc7005d928c12
valery <valery.fremaux@gmail.com>
2019-01-31 10:06:03 +0100

    fix travis version

--- version.php
+++ version.php
@@ -27,10 +27,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018022800; // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018042700; // Requires this Moodle version.
+$plugin->requires  = 2018112800; // Requires this Moodle version.
 $plugin->component = 'tool_cronmonitor'; // Full name of the plugin (used for diagnostics).
-$plugin->release = '3.5.0 (Build 2018022800)';
+$plugin->release = '3.6.0 (Build 2018022800)';
 $plugin->maturity = MATURITY_STABLE;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.5.0001';
+$plugin->codeincrement = '3.6.0001';
